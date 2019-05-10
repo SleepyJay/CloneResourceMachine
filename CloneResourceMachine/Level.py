03ff08e643e0fa8551f07fc21960195f3dc80168
Jason Agosta <agostaj@d20green.com>
2019-05-10 10:01:17 -0400

    CRM-xxx demote Goal back to named tuple (dirty check in)
    This will break stuff, because "expected" is no longer supported, but that has to be seriously rethought.

--- CloneResourceMachine/Level.py
+++ CloneResourceMachine/Level.py
@@ -3,16 +3,16 @@ from collections import namedtuple
 from JAGpy.Structs import lookup
 from JAGpy.Numbers import intify
 from CloneResourceMachine.Input import Input
-from CloneResourceMachine.Goal import Goal
 from CloneResourceMachine.Program import Program
 
 Registers = namedtuple('registers', 'count values')
+Goal = namedtuple('goal', 'formula size speed expected')
 ALWAYS_AVAILABLE = ['echo']
 
 
 class Level(object):
 
-    def __init__(self, key, data):
+    def __init__(self, key, data) -> None:
         self.original_data = data
         self.key = str(key)
         self.name = lookup(data, 'name')
@@ -20,10 +20,9 @@ class Level(object):
         self.programs = dict()
         self.available_cmds = []
 
-        self.goal = None
         self.input = None
+        self.goal = None
         self.registers = None
-        self.inboxes = []        # list of input tried
 
         self.process_data(data)
 
@@ -34,16 +33,48 @@ class Level(object):
         self.available_cmds = lookup(data, 'available', [])
         self.available_cmds.extend(ALWAYS_AVAILABLE)
 
-        self.goal = self.process_goal(lookup(data, 'goal'))
-        self.input = self.process_input(lookup(data, 'input'))
-        self.registers = self.process_registers(lookup(data, 'registers'))
-
-        self.process_programs(lookup(data, 'programs', dict()))
+        self.process_input(lookup(data, 'input'))
+        self.process_goal(lookup(data, 'goal'))
+        self.process_registers(lookup(data, 'registers'))
+        self.process_programs(lookup(data, 'programs'))
 
     def process_programs(self, programs_data):
+        if not programs_data:
+            return
+
         for key, program in programs_data.items():
             self.programs[key] = \
                 Program(self.key, key, program, self.available_cmds)
+
+    def process_registers(self, register_data):
+        if not register_data:
+            return Registers(0, [])
+
+        values = lookup(register_data, 'values', [])
+
+        new_vals = dict()
+
+        # string-ify all keys
+        for key in values:
+            new_vals[str(key)] = values[key]
+
+        return Registers(lookup(register_data, 'count', 0), new_vals)
+
+    def process_input(self, input_data):
+        if not input_data:
+            return
+
+        self.input = Input(input_data)
+
+    def process_goal(self, goal_data):
+        if not goal_data:
+            return
+
+        # "Expected" here is if there is ONE and ONLY ONE output for ANY GIVEN input
+        # (e.g. Level 3 in the main game has this property)
+        self.goal = Goal(
+            lookup(goal_data, 'formula'), lookup(goal_data, 'size'),
+            lookup(goal_data, 'speed'), lookup(goal_data, 'expected'))
 
     def get_program(self, key):
         if self.is_movie:
@@ -55,44 +86,5 @@ class Level(object):
             sol = lookup(self.programs, 'optimal')
 
         return sol
-
-    def process_registers(self, register_data):
-        if self.is_movie:
-            return
-
-        if not register_data:
-            return Registers(0, [])
-
-        values = lookup(register_data, 'values', [])
-
-        new_vals = dict()
-
-        for key in values:
-            new_vals[str(key)] = values[key]
-
-        return Registers(lookup(register_data, 'count', 0), values)
-
-    def process_input(self, input_data):
-        if self.is_movie:
-            return
-
-        if not input_data:
-            return
-
-        input_obj = Input(
-            lookup(input_data, 'alphabet'), lookup(input_data, 'count'), lookup(input_data, 'sample'))
-
-        return input_obj
-
-    def process_goal(self, goal_data):
-        if self.is_movie:
-            return
-
-        if not goal_data:
-            return
-
-        return Goal(
-            lookup(goal_data, 'formula'), lookup(goal_data, 'size'),
-            lookup(goal_data, 'speed'), lookup(goal_data, 'expected'))
 
 
